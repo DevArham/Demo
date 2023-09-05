f49c91b2191af024f73a7bf150bca711a7d2667d
Arham Riaz <ariaz@layer-one.com>
2023-09-05 17:00:41 +0500

    Sample project

---
+++ SampleSol/SampleSol/File1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public static class File1
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Hello World - File 1");
+        }
+    }
+}
---
+++ SampleSol/SampleSol/File3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public static class File3
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Hello World - File 3");
+        }
+    }
+}
